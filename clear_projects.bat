5312f21589e283ad259da59f97cfac7475cf7bd8
FasterMars <fastermars16@outlook.com>
2023-01-06 20:19:32 +0530

    Refresh Bismuth files

---
+++ clear_projects.bat
@@ -0,0 +1,8 @@
+del Bismuth.sln
+rmdir /s bin
+del Bismuth-Core\Bismuth-core.vcxproj
+del Bismuth-Core\Bismuth-core.vcxproj.filters
+del Bismuth-Core\Bismuth-core.vcxproj.user
+del Bismuth-Sandbox\Bismuth-Sandbox.vcxproj
+del Bismuth-Sandbox\Bismuth-Sandbox.vcxproj.user
+PAUSE
